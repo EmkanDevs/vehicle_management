7b2ca772b608390dd338534c75c5c906f6bd9c2f
EmkanDevs <emkan.erpnext@gmail.com>
2025-06-12 08:59:30 +0300

    new doctype

---
+++ vehicle_management/vehicle_management/doctype/transportation_service/transportation_service.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Finbyz and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Transportation Service", {
+// 	refresh(frm) {
+
+// 	},
+// });
